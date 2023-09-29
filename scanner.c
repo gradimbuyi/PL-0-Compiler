67165340a13454c0c1300b96bb695e1f4585e085
Gradi Mbuyi <gradimbuyi@outlook.com>
2023-09-29 06:47:07 -0400

    Add functions to performing the scanning. Must debug.

--- scanner.c
+++ scanner.c
@@ -2,72 +2,179 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-
 #include "scanner.h"
 
-char *RESERVED_WORDS[] = 
-{
-    "null", "beggin", "call", "const", "do", "else", "end", "if", "odd",
-    "procedure", "read", "then", "var", "while", "write"
-};
-
-char *LEXEME_TABLE[100];
+//char *LEXEME_TABLE[100];
+char *IDENTIFIER_NAMES[100];
+int NUMBER_TABLE[100];
 int TOKEN_LIST[100];
+
+int NUM_IDENTIFIER = 0;
+int TABLE_SIZE = 0;
 int NUM_TOKEN = 0;
 
-/*
-void readSourceProgram(char *file_name)
+int readSourceProgram(char *file_name, char **program_memory, int num_inputs)
 {
     FILE *input_file = fopen(file_name, "r");
-
     char buffer[strmax];
-
-    fprintf(stderr, "Source Program:\n");
 
     while(fgets(buffer, strmax, input_file)) 
     {
-        fprintf(stderr, "%s", buffer);
-        
+        program_memory[num_inputs] = malloc(sizeof(char) * strlen(buffer) + 1);
+        strcpy(program_memory[num_inputs], buffer);
+        num_inputs++;
     }
 
-    fprintf(stderr, "\n");
     fclose(input_file);
+    return num_inputs;
 }
-*/
 
-void scanLine(char *buffer, int buffer_length)
+void printSourceProgram(char **program_memory, int num_inputs)
 {
-    char temp;
+    fprintf(stderr, "Source Program:\n");
 
-    for(int i = 0; i < buffer_length; i++)
+    for(int i = 0; i < num_inputs; i++)
     {
-        //temp = buffer[i];
-        if(isalpha(buffer[i])) ;// do something
-       
+        fprintf(stderr, "%s", program_memory[i]);
     }
+}
+
+int checkTokenType(char *token, int isAllNumber, int foundInvalidSym) 
+{
+    int token_type;
+
+    if(isAllNumber == 0) 
+    {
+        int number;
+        sscanf(token, "%d", &number);
+        NUMBER_TABLE[TABLE_SIZE] = number;
+        TABLE_SIZE++;
+        token_type = numbersym;
+
+        if(number > imax) fprintf(stderr, "%s\t\tError: Too many digits", token);
+        else fprintf(stderr, "%s\t\t%d", token, token_type);
+
+    } else {
+
+        if(strcmp(token, "null") == 0)              token_type = nullsym;
+        else if(strcmp(token, "skip") == 0)         token_type = skipsym;  
+        else if(strcmp(token, "+") == 0)            token_type = plussym;
+        else if(strcmp(token, "-") == 0)            token_type = minussym;
+        else if(strcmp(token, "*") == 0)            token_type = multsym;
+        else if(strcmp(token, "/") == 0)            token_type = slashsym;
+        else if(strcmp(token, "ifel") == 0)         token_type = ifelsym;
+        else if(strcmp(token, "=") == 0)            token_type = eqlsym;
+        else if(strcmp(token, "<>") == 0)           token_type = neqsym;
+        else if(strcmp(token, "<") == 0)            token_type = lessym;
+        else if(strcmp(token, "<=") == 0)           token_type = leqsym;
+        else if(strcmp(token, ">") == 0)            token_type = gtrsym;
+        else if(strcmp(token, ">=") == 0)           token_type = geqsym; 
+        else if(strcmp(token, "(") == 0)            token_type = lparentsym;
+        else if(strcmp(token, ")") == 0)            token_type = rparentsym;
+        else if(strcmp(token, ",") == 0)            token_type = commasym;
+        else if(strcmp(token, ";") == 0)            token_type = semicolonsym;
+        else if(strcmp(token, ")") == 0)            token_type = periodsym;
+        else if(strcmp(token, ":=") == 0)           token_type = becomessym;
+        else if(strcmp(token, "begin") == 0)        token_type = beginsym;
+        else if(strcmp(token, "end") == 0)          token_type = endsym;
+        else if(strcmp(token, "if") == 0)           token_type = ifsym;
+        else if(strcmp(token, "then") == 0)         token_type = thensym;
+        else if(strcmp(token, "while") == 0)        token_type = whilesym; 
+        else if(strcmp(token, "do") == 0)           token_type = dosym;
+        else if(strcmp(token, "call") == 0)         token_type = callsym; 
+        else if(strcmp(token, "const") == 0)        token_type = constsym; 
+        else if(strcmp(token, "procedure") == 0)    token_type = procsym; 
+        else if(strcmp(token, "write") == 0)        token_type = writesym;
+        else if(strcmp(token, "read") == 0)         token_type = readsym; 
+        else if(strcmp(token, "else") == 0)         token_type = elsesym; 
+        else if(strcmp(token, "var") == 0)          token_type = varsym; 
+        else {
+            
+            token_type = identsym;
+
+            for(int i = 0; i < strlen(token); i++) if(!isalpha(token[i]) || !isdigit(token[i])) 
+            {
+                token_type = -1;
+                break;
+            }
+
+            IDENTIFIER_NAMES[NUM_IDENTIFIER] = malloc(sizeof(char) * strlen(token) + 1);
+            strcpy(IDENTIFIER_NAMES[NUM_IDENTIFIER], token);
+            NUM_IDENTIFIER++;
+        }
+
+        if(token_type == -1) fprintf(stderr, "%s\t\tError: Not a valid symbol", token);
+        else if(strlen(token) > strmax) fprintf(stderr, "%s\t\tError: Ident length too long", token);
+        else fprintf(stderr, "%s\t\t%d", token, token_type);
+    }
+    
+    return token_type;
 }
 
 void lexicalAnalyzer(char *file_name)
 {
-    FILE *input_file = fopen(file_name, "r");
-    char buffer[strmax];
+    int num_inputs = 0; 
+    int isAllNumber = 0;
+    int foundInvalidSym = -1;
 
-    fprintf(stderr, "Source Program:\n");
+    char **program_memory = malloc(sizeof(char *) * 100); 
 
-    while(fgets(buffer, strmax, input_file)) 
-    {
-        fprintf(stderr, "%s", buffer);
-        scanLine(buffer, strlen(buffer));
-    }
+    num_inputs = readSourceProgram(file_name, program_memory, num_inputs);
+    printSourceProgram(program_memory, num_inputs);
 
-    fprintf(stderr, "\n");
-    fclose(input_file);
-}
-
-void printLexemeTable() 
-{
     fprintf(stderr, "\nLexeme Table:\n\n");
     fprintf(stderr, "lexeme\ttoken type\n");
+    
+    for(int i = 0; i < num_inputs; i++) {
+        char token[strmax]; 
+        int token_size = 0;
+
+        for(int j = 0; j < strlen(program_memory[i]); j++)
+        {
+            if(program_memory[i][j] == '/' && j + 1 < strlen(program_memory[i]) && program_memory[i][j + 1] == '*')
+            {
+                char current = program_memory[i][j];
+                char next = program_memory[i][j + 1];
+
+                do {
+                    j += 2;
+
+                    if(j < strlen(program_memory[i]) && j + 1 < strlen(program_memory[i])) 
+                    {
+                        current = program_memory[i][j];
+                        next = program_memory[i][j + 1];
+                    }
+
+                    else if(j < strlen(program_memory[i]) && i < num_inputs)
+                    {
+                        current = program_memory[i][j];
+                        next = program_memory[i + 1][0];
+                        i++; j = 0;
+                    }
+
+                } while(current != '*' && next != '/');
+            }
+
+            else if(program_memory[i][j] == ' ' || program_memory[i][j] == '\n' && program_memory[i][j] == '\t' && token_size != 0)
+            {
+                TOKEN_LIST[NUM_TOKEN] = checkTokenType(token, isAllNumber, foundInvalidSym);
+                NUM_TOKEN++;
+
+                token_size = 0;
+                isAllNumber = 0;
+                foundInvalidSym = -1;
+            }
+
+            else if(program_memory[i][j] != ' ' || program_memory[i][j] != '\n' || program_memory[i][j] != '\t')
+            {
+                if(isAllNumber == 0 && !isdigit(program_memory[i][j])) isAllNumber = -1;
+                token[token_size] = program_memory[i][j];
+                token[token_size + 1] = '\0'; 
+            }
+        }
+    }
+
+    free(program_memory);
 }
 
 int main(int argc, char *argv[]) 
@@ -81,7 +188,6 @@ int main(int argc, char *argv[])
     else 
     {
         lexicalAnalyzer(argv[1]);
-        printLexemeTable();
     }
 
     return 0;
--- scanner.h
+++ scanner.h
@@ -8,13 +8,16 @@
 
 typedef enum 
 { 
-    nulsym = 1, idsym, numbersym, plussym, minussym, multsym, slashsym, oddsym,
-    eqsym, neqsym, lessym, leqsym, gtrsym, geqsym, lparentsym, rparentssym, commasym, 
-    semicolonsym, periodsym, becomessym, beginsym, endsym, ifsym, thensym, whilesym,
-    dosym, callsym, constsym, varsym, procsym, writesym
+    nullsym = 0, skipsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5,
+    multsym = 6, slashsym = 7, ifelsym = 8, eqlsym = 9, neqsym = 10, lessym = 11,
+    leqsym = 12, gtrsym = 13, geqsym = 14, lparentsym = 15, rparentsym = 16,
+    commasym = 17, semicolonsym = 18, periodsym = 19, becomessym = 20,
+    beginsym = 21, endsym = 22, ifsym = 23, thensym = 24, whilesym = 25, dosym = 26,
+    callsym = 27, constsym = 28, varsym = 29, procsym = 30, writesym = 31,
+    readsym = 32, elsesym = 33
 } token_type;
 
-void readSourceProgram(char *file_name);
+int readSourceProgram(char *file_name, char **program_memory, int num_inputs);
 void printLexemeTable();
 
 #endif
